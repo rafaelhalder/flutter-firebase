08c1a4849f3e3e9f728f6d23b0e2e9ecb96ea7e0
rafaelhalder <rafaelhalder@gmail.com>
2022-02-23 17:20:32 -0300

    Start UI

--- lib/main.dart
+++ lib/main.dart
@@ -45,14 +45,61 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Column(
         mainAxisAlignment:MainAxisAlignment.center,
         crossAxisAlignment:CrossAxisAlignment.start,
-        children: const [
-          Text(
+        children: [
+          const Text(
             "My App Title",
             style: TextStyle(
               color: Colors.black,
               fontSize: 28.0,
               fontWeight: FontWeight.bold,
             ),
+          ),
+          const Text(
+            'Login to Your App',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44.0,
+              fontWeight: FontWeight.bold,
+            )
+          ),
+          const SizedBox(
+            height:44.0,
+          ),
+          const TextField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              hintText: 'User Email',
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
+
+            )
+          ),
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
+            )
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text("Don't remember your password?", style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 80.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color.fromARGB(255, 0, 106, 255),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0)
+              ),
+              onPressed: () {},
+              child: const Text('Login', style: TextStyle(color: Colors.white, fontSize:18.0)),
+              ),
           )
         ],
       )
