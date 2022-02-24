d23b1d953e86fa0b61f16c05bd4416ffd755d016
Rafael Henrique da Silva <31450046+rafaelhalder@users.noreply.github.com>
2022-02-23 22:49:12 -0300

    basic auth with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -69,10 +69,12 @@ class _LoginScreenState extends State<LoginScreen> {
           email: email, password: password);
       user = userCredential.user;
     } on FirebaseAuthException catch (e) {
-      if (e.code == "user-not-fount") {
-        print("No User found for that email");
+      if (e.code == "user-not-found") {
+        print(e);
       }
     }
+
+    return user;
   }
 
   @override
@@ -83,76 +85,82 @@ class _LoginScreenState extends State<LoginScreen> {
     TextEditingController _passwordController = TextEditingController();
 
     return Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "My App Title",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "My App Title",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
             ),
-            const Text('Login to Your App',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 44.0,
-                  fontWeight: FontWeight.bold,
-                )),
-            const SizedBox(
-              height: 44.0,
+          ),
+          const Text(
+            'Login to Your App',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 44.0,
+              fontWeight: FontWeight.bold,
             ),
-            TextField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'User Email',
-                  prefixIcon: Icon(Icons.mail, color: Colors.black),
-                )),
-            TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText: "User Password",
-                  prefixIcon: Icon(Icons.lock, color: Colors.black),
-                )),
-            const SizedBox(
-              height: 12.0,
+          ),
+          const SizedBox(
+            height: 44.0,
+          ),
+          TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'User Email',
+              prefixIcon: Icon(Icons.mail, color: Colors.black),
             ),
-            const Text(
-              "Don't remember your password?",
-              style: TextStyle(color: Colors.blue),
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: const InputDecoration(
+              hintText: "User Password",
+              prefixIcon: Icon(Icons.lock, color: Colors.black),
             ),
-            const SizedBox(
-              height: 80.0,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: RawMaterialButton(
-                fillColor: const Color.fromARGB(255, 0, 106, 255),
-                elevation: 0.0,
-                padding: const EdgeInsets.symmetric(vertical: 20.0),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.0)),
-                onPressed: () async {
+          ),
+          const SizedBox(
+            height: 12.0,
+          ),
+          const Text(
+            "Don't remember your password?",
+            style: TextStyle(color: Colors.blue),
+          ),
+          const SizedBox(
+            height: 80.0,
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: RawMaterialButton(
+              fillColor: const Color.fromARGB(255, 0, 106, 255),
+              elevation: 0.0,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0)),
+              onPressed: () async {
 //test app
-                  User? user = await loginUsingEmailPassword(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                      context: context);
-                      print(user);
-                      if(user != null){
-                        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const ProfileScreen()));
-                      }
-                },
-                child: const Text('Login',
-                    style: TextStyle(color: Colors.white, fontSize: 18.0)),
-              ),
-            )
-          ],
-        ));
+                User? user = await loginUsingEmailPassword(
+                    email: _emailController.text,
+                    password: _passwordController.text,
+                    context: context);
+                print(user);
+                if (user != null) {
+                  Navigator.of(context).pushReplacement(MaterialPageRoute(
+                      builder: (context) => const ProfileScreen()));
+                }
+              },
+              child: const Text('Login',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0)),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
